ba1021a3300067814b6fdf5f2be4bc396a3f0ac1
ikaros <ikaros@js-coder.com.tw>
2022-10-31 00:08:36 +0800

    Add record for lending

--- initial.sql
+++ initial.sql
@@ -11,3 +11,19 @@ create table moneybook
 
 alter table moneybook owner to postgres;
 
+create table lending
+(
+    id        serial
+        constraint lending_pk
+            primary key,
+    target    varchar                  not null,
+    price     integer                  not null,
+    content   text                     not null,
+    remark    text                     not null,
+    time      timestamp with time zone not null,
+    is_payoff boolean default false    not null
+);
+
+alter table lending
+    owner to postgres;
+
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ router.post('/', async (ctx) => {
 
   let timeNow = dayjs();
   const timeInRequest = dayjs(ctx.request.body.time);
-  if(ctx.request.body.time && timeInRequest.isValid()) timeNow = timeInRequest;
+  if (ctx.request.body.time && timeInRequest.isValid()) timeNow = timeInRequest;
 
   if (isNaN(price) || price < 0) {
     return ctx.throw(400, '價格需為正整數');
@@ -29,6 +29,50 @@ router.post('/', async (ctx) => {
     remark,
     time: timeNow.toISOString(),
   };
+});
+
+router.post('/lending', async (ctx) => {
+  const target = ctx.request.body.target;
+  const price = parseInt(ctx.request.body.price);
+  const content = ctx.request.body.content;
+  const remark = ctx.request.body.remark ?? null;
+
+  let timeNow = dayjs();
+  const timeInRequest = dayjs(ctx.request.body.time);
+  if (ctx.request.body.time && timeInRequest.isValid()) timeNow = timeInRequest;
+
+  if (isNaN(price) || price < 0) {
+    return ctx.throw(400, '價格需為正整數');
+  }
+
+  await sql`INSERT INTO lending (target, price, content, time, remark) VALUES (${target}, ${price}, ${content}, ${timeNow.toISOString()}, ${remark})`;
+
+  ctx.body = {
+    target,
+    price,
+    content,
+    remark,
+    time: timeNow.toISOString(),
+  };
+});
+
+router.get('/lending', async (ctx) => {
+  const results = await sql`
+    SELECT target, price, content, time, remark
+    FROM lending
+    WHERE is_payoff = FALSE
+    ORDER BY time DESC;
+  `;
+
+  ctx.body = results.map((r) => {
+    return {
+      target: r.target,
+      price: parseInt(r.price),
+      content: r.content,
+      time: dayjs(r.time).toISOString(),
+      remark: r.remark,
+    };
+  });
 });
 
 app.use(router.routes()).use(router.allowedMethods());
